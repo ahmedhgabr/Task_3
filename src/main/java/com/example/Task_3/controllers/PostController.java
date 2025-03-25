b8558923a4272fd1f4fdb94ca1702674cc5495a1
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-03-26 00:50:43 +0200

    controller

--- src/main/java/com/example/Task_3/controllers/PostController.java
+++ src/main/java/com/example/Task_3/controllers/PostController.java
@@ -1,46 +1,62 @@
-//package com.example.Task_3.controllers;
-//
-//import com.example.Task_3.services.PostService;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//import java.util.Map;
-//
-//@RestController
-//@RequestMapping("/posts")
-//public class PostController {
-//    private final PostService postService;
-//    public PostController(PostService postService) {
-//        this.postService = postService;
-//    }
-//
-//    @GetMapping
-//    public List<Post> getAllPosts() {
-//        return postService.getAllPosts();
-//    }
-//
-//    @PostMapping
-//    public Post createPost(@RequestBody Post post) {
-//        return postService.savePost(post);
-//    }
-//
-//    @DeleteMapping
-//    public ResponseEntity<Void> deletePost(@RequestBody Map<String,String> body) {
-//        postService.deletePost(body.get("id"));
-//        return ResponseEntity.noContent().build();
-//    }
-//
-//    @GetMapping("/{id}")
-//    public ResponseEntity<Post> getPostById(@PathVariable("id") String id) {
-//        if(!postService.getPostById(id).isPresent())
-//            return ResponseEntity.notFound().build();
-//        return ResponseEntity.ok(postService.getPostById(id).get());
-//    }
-//
-//    //Get Posts by Author ID
-//
-//
-//    //Add Comment to post
-//
-//}
+package com.example.Task_3.controllers;
+
+import com.example.Task_3.models.Comment;
+import com.example.Task_3.models.Post;
+import com.example.Task_3.services.PostService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping
+    public List<Post> getAllPosts() {
+        return postService.getAllPosts();
+    }
+
+    @PostMapping
+    public Post createPost(@RequestBody Post post) {
+        return postService.savePost(post);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deletePost(@RequestBody Map<String, String> body) {
+        postService.deletePost(body.get("id"));
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Post> getPostById(@PathVariable("id") String id) {
+        if (!postService.getPostById(id).isPresent())
+            return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(postService.getPostById(id).get());
+    }
+
+    //Get Posts by Author ID
+    @GetMapping("/postsByUser/{authorID}")
+    public List<Post> getPostsByAuthorID(@PathVariable("authorID") String authorID) {
+        return postService.getPostsByAuthorID(authorID);
+    }
+
+
+    //Add Comment to post
+    @PostMapping("/{postID}/comments")
+    public ResponseEntity<Post> addCommentToPost(@PathVariable("postID") String postID, @RequestBody Map<String, String> body) {
+        Comment comment = new Comment(body.get("text"), body.get("date"));
+        Post post = postService.addCommentToPost(postID, comment);
+        if (post == null)
+            return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(post);
+    }
+
+
+}
--- src/main/java/com/example/Task_3/controllers/UserController.java
+++ src/main/java/com/example/Task_3/controllers/UserController.java
@@ -1,48 +1,59 @@
-//package com.example.Task_3.controllers;
-//
-//import com.example.Task_3.services.UserService;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//import org.springframework.web.server.ResponseStatusException;
-//
-//import java.util.List;
-//import java.util.Map;
-//import java.util.Optional;
-//
-//@RestController
-//@RequestMapping("/users")
-//public class UserController {
-//    private final UserService userService;
-//    public UserController(UserService userService) {
-//        this.userService = userService;
-//    }
-//
-//    @GetMapping
-//    public List<User> getAllUsers() {
-//        return userService.getAllUsers();
-//    }
-//
-//    @PostMapping
-//    public User createUser(@RequestBody User user) {
-//        return userService.save(user);
-//    }
-//
-//    @DeleteMapping
-//    public void deleteUser(@RequestBody Map<String,String> body) {
-//        userService.deleteUserById(body.get("id"));
-//    }
-//
-//    @GetMapping("/{id}")
-//    public ResponseEntity getUserById(@PathVariable("id") String id) {
-//        Optional<User> user = userService.findUserById(id);
-//        if(user.isPresent()) {
-//            return ResponseEntity.ok(user.get());
-//        }
-//        else
-//            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"user not found");
-//    }
-//
-//    //Update Username of the User
-//
-//}
+package com.example.Task_3.controllers;
+
+import com.example.Task_3.models.User;
+import com.example.Task_3.services.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.save(user);
+    }
+
+    @DeleteMapping
+    public void deleteUser(@RequestBody Map<String, String> body) {
+        userService.deleteUserById(body.get("id"));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getUserById(@PathVariable("id") String id) {
+        Optional<User> user = userService.findUserById(id);
+        if (user.isPresent()) {
+            return ResponseEntity.ok(user.get());
+        } else
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "user not found");
+    }
+
+    //Update Username of the User
+    @PutMapping("/updateUser")
+    public User updateUserUsername(@RequestBody Map<String, String> body) {
+        try {
+            User user = userService.updateUserUsername(body.get("id"), body.get("username"));
+            return user;
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "user not found");
+        }
+
+    }
+
+}
