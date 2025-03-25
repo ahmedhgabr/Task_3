34afe1f6a6f966b11da0152dc922b0786585f344
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-03-26 00:53:59 +0200

    services

--- src/main/java/com/example/Task_3/services/PostService.java
+++ src/main/java/com/example/Task_3/services/PostService.java
@@ -1,41 +1,60 @@
-//package com.example.Task_3.services;
-//
-//import com.example.Task_3.models.Comment;
-//import com.example.Task_3.models.Post;
-//import com.example.Task_3.repositories.PostRepository;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.stereotype.Service;
-//import org.springframework.web.server.ResponseStatusException;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//@Service
-//public class PostService {
-//
-//    private final PostRepository postRepository;
-//
-//    public PostService(PostRepository postRepository) {
-//        this.postRepository = postRepository;
-//    }
-//
-//    public Post savePost(Post post) {
-//        return postRepository.save(post);
-//    }
-//    public List<Post> getAllPosts() {
-//        return postRepository.findAll();
-//    }
-//
-//    public Optional<Post> getPostById(String id) {
-//        return postRepository.findById(id);
-//    }
-//
-//    public void deletePost(String id) {
-//        postRepository.deleteById(id);
-//    }
-//
-//    //Get Posts By Author ID
-//
-//    //Add Comment to Post
-//
-//}
+package com.example.Task_3.services;
+
+import com.example.Task_3.models.Comment;
+import com.example.Task_3.models.Post;
+import com.example.Task_3.repositories.PostRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    private final PostRepository postRepository;
+
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public Post savePost(Post post) {
+        //check if the author is null or not
+        if (post.getAuthor() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Author is Required");
+        }
+        Post newPost = new Post(post.getTitle(), post.getContent(), post.getAuthor());
+        return postRepository.save(newPost);
+    }
+
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+    }
+
+    public Optional<Post> getPostById(String id) {
+        return postRepository.findById(id);
+    }
+
+    public void deletePost(String id) {
+        postRepository.deleteById(id);
+    }
+
+    //Get Posts By Author ID
+    public List<Post> getPostsByAuthorID(String authorID) {
+        List<Post> posts = postRepository.findByAuthorID(authorID);
+        return posts;
+    }
+
+    //Add Comment to Post
+    public Post addCommentToPost(String postID, Comment newComment) {
+        Optional<Post> post = postRepository.findById(postID);
+        if (post.isPresent()) {
+            post.get().addComment(newComment);
+            return postRepository.save(post.get());
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Post Not Found");
+        }
+    }
+
+}
